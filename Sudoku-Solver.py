839d8f05d7bf358173526f31b572d20615ce64de
Jason <jyan575@aucklanduni.ac.nz>
2022-03-13 15:46:55 +1300

    Pulled isValidRow function from main

--- Sudoku-Solver.py
+++ Sudoku-Solver.py
@@ -25,3 +25,41 @@ def findEmpty(board):
 
     return None
 
+
+def isValidRow(bo, num, pos):
+    row = pos[0];
+    col = pos[1];
+    for j in range(len(bo)):
+        if (bo[row][j] == num) and (j != col) :
+            return False
+    return True
+
+
+def printBoard(bo):
+
+    # Set up a nested for loop to traverse the board
+    for i in range(len(bo)):
+
+        # each 3x3 box of the sudoku should be separated by lines
+        # the % operator lets us check if the index at a given point is divisible by 3
+        # letting us know when to place a divider
+        if i % 3 == 0 and i != 0:
+            # place dividers for rows
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                # place dividers for columns
+                print(" | ", end="")
+                # end is to prevent new line printing
+
+            if j == 8:
+                print(bo[i][j])
+                # allow new line at the 8th column to allow new row to print
+            else:
+                print(str(bo[i][j]) + " ", end="")
+                # otherwise we just print our numbers in between the dividers
+
+
+printBoard(testBoard)
+
